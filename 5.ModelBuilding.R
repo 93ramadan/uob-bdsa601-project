13a2281c91c3a60f6399835a8637d43aa659f907
aishakhalidm <aisha.khalid.m@gmail.com>
2020-12-10 00:42:50 +0300

    Step 3 completed

--- 5.ModelBuilding.R
+++ 5.ModelBuilding.R
@@ -3,4 +3,24 @@
 #*  Build Logistic Growth Model using Existing Data
 #************************************************************#
 
-# now imagine you made this change and you want to commit..
+#The predicted number of cases over days is obtained using 
+#the logistic growth model defined as
+# 𝒚=𝝋𝟏𝟏+𝒆−(𝒕 − 𝝋𝟐𝝋𝟑) 
+
+#This model is used based on the exponential growth of the existing data the best 
+#model is the logistic growth model for its ability of limit the increase to a value
+#(population) rather than increasing to infinite.
+
+#y is the number of cases at day t
+# phi1 is the upper limit (maximum) for y 
+# 1/phi3 is a non-negative value represents the daily growth rate
+#---------------------------------------------------------------#
+# 
+model = nls(total_cases~SSlogis(dayCounter,phi1,phi2,phi3),data=TCTDperDay)
+summary(model)
+
+# calculating the growth rate of the cases:
+
+phi = coef(model)
+DailyGrowthRate = round((1/phi[3] ), 3)
+print(paste("The growth rate of the cases is ", DailyGrowthRate))
