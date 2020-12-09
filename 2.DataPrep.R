b490594ff30b6824fc07edf829cd9ed716fd8f76
Ahmed Khedr <83ahmed@windowslive.com>
2020-12-09 18:18:00 +0300

    create files for each data analysis task

---
+++ 2.DataPrep.R
@@ -0,0 +1,11 @@
+#************************************************************#
+#*  Convert "date" to DATE data type
+#************************************************************#
+bahrain$date_asdate = as.Date(bahrain$date, "%m/%d/%Y")
+
+#************************************************************#
+#*  Remove days which have no registered total cases
+#************************************************************#
+which_casesNotZero = bahrain$total_cases!=0
+#* if a day comes in the middle with total cases as 0, will this cause a error in the analysis since this is time-based analysis?
+bahrain = bahrain[which_casesNotZero,]
--- main.R
+++ main.R
@@ -7,26 +7,9 @@
 #*    2. Ahmed Khedr
 #************************************************************#
 
+source("0.Imports.R")
+source("1.DataLoad.R")
+source("2.DataPrep.R")
+source("3.BasicAnalysis.R")
 
-#************************************************************#
-#*  Reading & Visualize Data
-#************************************************************#
-bahrain = read.csv('bahrain.csv')
 
-# Remove days which have no registered total cases
-which_casesNotZero = bahrain$total_cases!=0
-#* if a day comes in the middle with total cases as 0, will this cause a error in the analysis since this is time-based analysis?
-bahrain = bahrain[which_casesNotZero,]
-
-# Convert to Date data type
-bahrain$date_asdate = as.Date(bahrain$date, "%m/%d/%Y")
-
-# Study Time-frame (Study Data)
-study_timeframe_start = min(bahrain$date_asdate)
-study_timeframe_end = max(bahrain$date_asdate)
-study_timeframe = study_timeframe_end - study_timeframe_start
-study_timeframe
-
-# KPIs
-dateof_first_case = min(bahrain[which(bahrain$total_cases!=0),]$date_asdate)
-dateof_first_death = min(bahrain[which(bahrain$total_deaths!=0),]$date_asdate)
