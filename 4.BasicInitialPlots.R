aeaf1dffeec6355877778af2bfba8c6105fd3a1b
Ahmed Khedr <83ahmed@windowslive.com>
2020-12-09 20:41:44 +0300

    time fix

--- 4.BasicInitialPlots.R
+++ 4.BasicInitialPlots.R
@@ -7,14 +7,14 @@ TCTDperDay = data.frame(cbind("day" = dayCounter, "total_cases" = bahrain$total_
 
 InitialPlots.TotalCases = ggplot(TCTDperDay, aes(x = dayCounter)) + ggtitle("Kingdom of Bahrain") +
   geom_area(aes(y = total_cases), fill = "blue", alpha = 0.2) +
-  geom_line(aes(x = time, y = total_cases), color = "blue") +
+  geom_line(aes(x = dayCounter, y = total_cases), color = "blue") +
   xlab(paste("Time Period:", format(StudyTimeFrame.Start, "%d %B %Y"), "–" , format(StudyTimeFrame.End, "%d %B %Y"))) +
   ylab("Total Cases")
 
 
 InitialPlots.TotalDeaths = ggplot(TCTDperDay, aes(x = dayCounter)) + ggtitle("Kingdom of Bahrain") +
   geom_area(aes(y = total_deaths), fill = "black", alpha = 0.2) +
-  geom_line(aes(x = time, y = total_deaths), color = "black") +
+  geom_line(aes(x = dayCounter, y = total_deaths), color = "black") +
   xlab(paste("Time Period:", format(StudyTimeFrame.Start, "%d %B %Y"), "–" , format(StudyTimeFrame.End, "%d %B %Y")))+
   ylab("Total Deaths")
 
