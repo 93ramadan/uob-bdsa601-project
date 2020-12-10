ffc44ed35030374101ba5e696490769f06d74507
aishakhalidm <aisha.khalid.m@gmail.com>
2020-12-10 22:57:25 +0300

    step 5 and 6 are done

--- 6.ModelPrediction.R
+++ 6.ModelPrediction.R
@@ -2,3 +2,29 @@
 #************************************************************#
 #*  Generating & Plotting Predictions using Model
 #************************************************************#
+#*
+#* Predicting the number of total cases for the next three months (90 days)
+#* Specifically from 30th July 2020 to 16th October 2020
+#* using the fitted logistic growth model 
+
+#Predict the number of cases for three  months (90 days)
+ProjectionPeriod = 90 #in days
+Projected_DayCounter = (StudyTimeFrame.TotalDays +1) : (StudyTimeFrame.TotalDays +  ProjectionPeriod)
+Projected_total_cases = predict(model,data.frame(dayCounter=Projected_DayCounter))
+
+
+# increase the X-axis and Y-axis to occupies both real and predicted data
+dayCounterLimit = c(TCTDperDay$day,Projected_DayCounter)
+totalCasesLimit= c(TCTDperDay$total_cases,Projected_total_cases)
+
+#Plotting both real and predicted data of total cases in one graph
+
+plot(total_cases~day,data=TCTDperDay,pch=16,col="darkgrey",xlim=range(dayCounterLimit),ylim=range(totalCasesLimit),
+     main="Logistic Growth Model of COVID-19 in Kingdom of Bahrain",xlab="Days",ylab="Total Cases")
+lines(dayCounter,phi[1]/(1+exp(-(dayCounter-phi[2])/phi[3])),col="blue",lwd=2)
+lines(Projected_DayCounter,Projected_total_cases,col="red",lwd=2)
+
+grid( )
+legend("topleft",fill=c("darkgrey","blue","red"),c("data","fitted model","projected cases"))
+
+
---
+++ 8.HospitalizationRate.R
@@ -0,0 +1,24 @@
+# Step 6
+#******************************************************************#
+#*  Using the reported number of active cases and those receiving 
+#*  treatments, the hospitalization rate was estimated simply as 
+#*  the ratio of the treated cased to active cases. Afterwards,
+#*  the number of the predicted hospitalized cases was estimated.
+#******************************************************************#
+
+
+ActiveCases = 2832 # Ministry of Health 
+Treatment = 81 # Ministry of Health
+
+hospitalized = round(Treatment/ActiveCases,2) # hospitalized = 0.03
+hospitalized_cases = hospitalized*Projected_total_cases
+
+par(mfrow=c(1,2))
+
+plot(Projected_DayCounter,Projected_total_cases,pch=20,col="blue", xlab="Projected Time (3 months)",ylab="Projected COVID-19 Cases")
+grid( )
+
+plot(Projected_DayCounter,hospitalized_cases,pch=20,col="red", xlab="Projected Time (3 months)",ylab="Projected Hospitalized COVID-19 Cases")
+grid( )
+text(180,1400,paste("Available Beds",total_available_beds),col="red")
+
